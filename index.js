73933c4fa558dfae1a38753e040f393c38f6b7a9
variousnabil <varionabilr@gmail.com>
2020-07-13 10:02:13 +0700

    axes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const w = 825;
-const h = 400;
-const padding = 30;
+const w = 925;
+const h = 600;
+const paddingHorizontal = 75;
+const paddingVertical = 30;
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 let dataset = [];
-
+const arrYears = [];
+for (let i = 1950; i <= 2015; i += 5) {
+    arrYears.push(i);
+}
+console.log(arrYears);
 
 const svg = d3.select('.container')
     .append('svg')
@@ -11,8 +16,8 @@ const svg = d3.select('.container')
     .attr('height', h);
 
 const title = svg.append('text')
-    .attr('x', 400)
-    .attr('y', padding)
+    .attr('x', 450)
+    .attr('y', paddingVertical)
     .attr('id', 'title')
     .attr('text-anchor', 'middle')
     .style('font-size', '30px')
@@ -25,11 +30,15 @@ fetch(url)
 
         const xScale = d3.scaleLinear();
         xScale.domain([d3.min(dataset, d => d[0]), d3.max(dataset, d => d[0])]);
-        xScale.range([padding, w - padding]);
+        xScale.range([paddingHorizontal, w - paddingHorizontal]);
 
         const yScale = d3.scaleLinear();
         yScale.domain([0, d3.max(dataset, d => d[1])]);
-        yScale.range([0, h - padding]);
+        yScale.range([0, h - (paddingVertical * 4)]);
+
+        const yAxisScale = d3.scaleLinear();
+        yAxisScale.domain([0, d3.max(dataset, d => d[1])]);
+        yAxisScale.range([h - (paddingVertical * 4), 0]);
 
         const rect = svg.selectAll('rect')
             .data(dataset)
@@ -37,36 +46,21 @@ fetch(url)
             .append('rect')
             .attr('width', 2)
             .attr('height', d => yScale(d[1]))
-            .attr('x', (d, i) => padding + i * 2.78)
-            .attr('y', d => h - padding - yScale(d[1]))
+            .attr('x', (d, i) => paddingHorizontal + i * 2.82)
+            .attr('y', d => h - paddingVertical * 2 - yScale(d[1]))
             .attr('fill', 'green');
 
-        const xAxis = d3.axisBottom(xScale);
-        const yAxis = d3.axisLeft(yScale);
+        const xAxis = d3.axisBottom(xScale).tickValues(arrYears);
+        const yAxis = d3.axisLeft(yAxisScale);
 
         svg.append('g')
             .attr('id', 'x-axis')
-            .attr('transform', `translate(0, ${h - padding})`)
+            .attr('transform', `translate(0, ${h - paddingVertical * 2})`)
             .call(xAxis);
 
         svg.append('g')
             .attr('id', 'y-axis')
-            .attr('transform', `translate(${padding}, 0)`)
+            .attr('transform', `translate(${paddingHorizontal}, ${paddingVertical * 2})`)
             .call(yAxis);
     })
     .catch(err => console.log(err));
-
-
-// ignore
-// const xAxis = d3.axisBottom(xScale);
-// const yAxis = d3.axisLeft(yScale);
-
-// svg.append('g')
-//     .attr('id', 'x-axis')
-//     .attr('transform', `translate(0, ${h - padding})`)
-//     .call(xAxis);
-
-// svg.append('g')
-//     .attr('id', 'y-axis')
-//     .attr('transform', `translate(${padding}, 0)`)
-//     .call(yAxis);
